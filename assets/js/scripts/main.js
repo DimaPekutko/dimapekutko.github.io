0045a94f275a4d42c18b46f154dfea36c473df2f
Dmitry Pekutko <dkefir03@gmail.com>
2019-08-28 16:51:30 +0300

    Accordion block

--- assets/js/scripts/main.js
+++ assets/js/scripts/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     //slider
 
-    var slider_speed = 4000;
+    var slider_speed = 8000;
 
     $(".slider_left_carousel").slick({
     	infinite: true,
@@ -36,4 +36,44 @@ $(document).ready(function() {
 	    function sliderAutoPlay() {
 	        $('#next_slider_arrow').click()
 	    }
+
+    //accordion
+
+    var accord_btns = $('.accord_btn');
+    var accord_els = $('.accord_el');
+    var accord_sub_texts = $('.accord_sub_text');
+    var accord_texts = $('.accord_text');
+ 
+    for(var i = 0; i < accord_btns.length; i++) { 
+        var accord_btn = $(accord_btns[i]);
+        var count = i;
+        accord_btn.click({'i': i}, accordion);     
+    }
+
+    function accordion(e) {
+            var i = e.data.i;
+            var accord_el = $(accord_els[i]);
+            var accord_sub_text = $(accord_sub_texts[i]);
+            var accord_text = $(this.nextElementSibling);
+
+            if(accord_text.css('display') == 'block') {
+                accord_text.hide(300);
+                accord_el.css('color', '#FFFFFF');
+                accord_el.css('background', 'url(assets/images/accord_disabled_el.png)');
+                accord_sub_text.css('color', '#86C2FF');
+            }
+            else if(accord_text.css('display') == 'none') {
+                accord_els.css('color', '#FFFFFF');
+                accord_els.css('background', 'url(assets/images/accord_disabled_el.png)');
+                accord_sub_texts.css('color', '#86C2FF');
+                accord_texts.hide(300);
+
+                accord_text.show(300);
+                accord_el.css('color', '#0063FF');
+                accord_el.css('background', 'url(assets/images/accord_enabled_el.png)');
+                accord_sub_text.css('color', '#FFFFFF');
+
+            }  
+    }
+
 });
