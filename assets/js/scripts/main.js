de0674285f506b0e293c46cffd6c5f7350aafb32
Dmitry Pekutko <dkefir03@gmail.com>
2019-09-08 00:18:49 +0300

    Final3

--- assets/js/scripts/main.js
+++ assets/js/scripts/main.js
@@ -43,44 +43,55 @@ $(document).ready(function() {
        $('#next_slider_arrow').click()
    }
 
-    //accordion
+   //second section slider (only in mobile).
 
-    var accord_btns = $('.accord_btn');
-    var accord_els = $('.accord_el');
-    var accord_sub_texts = $('.accord_sub_text');
-    var accord_texts = $('.accord_text');
-    
-    for(var i = 0; i < accord_btns.length; i++) { 
-        var accord_btn = $(accord_btns[i]);
-        var count = i;
-        accord_btn.click({'i': i}, accordion);     
+   if($(document).width() < 768) {
+    $('.second_slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+}
+
+   //accordion
+
+   var accord_btns = $('.accord_btn');
+   var accord_els = $('.accord_el');
+   var accord_sub_texts = $('.accord_sub_text');
+   var accord_texts = $('.accord_text');
+
+   for(var i = 0; i < accord_btns.length; i++) { 
+    var accord_btn = $(accord_btns[i]);
+    accord_btn.click({'i': i}, accordion);     
+}
+
+function accordion(e) {
+    var i = e.data.i;
+    var accord_el = $(accord_els[i]);
+    var accord_sub_text = $(accord_sub_texts[i]);
+    var accord_text = $(this.nextElementSibling);
+
+    if(accord_text.css('display') == 'block') {
+        accord_text.hide(300);
+        accord_el.css('color', '#FFFFFF');
+        accord_el.css('background', 'url(assets/images/accord_disabled_el.png)');
+        accord_sub_text.css('color', '#86C2FF');
     }
+    else if(accord_text.css('display') == 'none') {
+        accord_els.css('color', '#FFFFFF');
+        accord_els.css('background', 'url(assets/images/accord_disabled_el.png)');
+        accord_sub_texts.css('color', '#86C2FF');
+        accord_texts.hide(300);
 
-    function accordion(e) {
-        var i = e.data.i;
-        var accord_el = $(accord_els[i]);
-        var accord_sub_text = $(accord_sub_texts[i]);
-        var accord_text = $(this.nextElementSibling);
+        accord_text.show(300);
+        accord_el.css('color', '#0063FF');
+        accord_el.css('background', 'url(assets/images/accord_enabled_el.png)');
+        accord_sub_text.css('color', '#FFFFFF');
 
-        if(accord_text.css('display') == 'block') {
-            accord_text.hide(300);
-            accord_el.css('color', '#FFFFFF');
-            accord_el.css('background', 'url(assets/images/accord_disabled_el.png)');
-            accord_sub_text.css('color', '#86C2FF');
-        }
-        else if(accord_text.css('display') == 'none') {
-            accord_els.css('color', '#FFFFFF');
-            accord_els.css('background', 'url(assets/images/accord_disabled_el.png)');
-            accord_sub_texts.css('color', '#86C2FF');
-            accord_texts.hide(300);
-
-            accord_text.show(300);
-            accord_el.css('color', '#0063FF');
-            accord_el.css('background', 'url(assets/images/accord_enabled_el.png)');
-            accord_sub_text.css('color', '#FFFFFF');
-
-        }  
-    }
+    }  
+}
 
     accord_btns[0].click(); // just to show first
 
@@ -95,5 +106,32 @@ $(document).ready(function() {
         prevArrow: $('#seventh_s_left_arrow'),
         nextArrow: $('#seventh_s_right_arrow'),
     });
-    
+
+    //ninth section news hover
+
+    if($(document).width() > 768) {
+
+        var newsposts = $('.newsblog_post');
+        var newsposts_images = $('.newsblog_post img');
+
+        for(var i = 0; i < newsposts.length; i++) {
+            var newpost = $(newsposts[i]);
+            newpost.mouseenter({'i': i}, ninthMouseenter);
+            newpost.mouseleave({'i': i}, ninthMouseleave);
+        }
+
+        function ninthMouseenter(e) {
+            var i = e.data.i;
+            $(newsposts_images[i]).css("opacity", "0.8");
+            $(newsposts[i]).find('.newsblog_share_button').show();
+        }
+
+        function ninthMouseleave(e) {
+            var i = e.data.i;
+            $(newsposts_images[i]).css("opacity", "1");
+            $(newsposts[i]).find('.newsblog_share_button').hide();
+        }
+
+    }
+
 });
